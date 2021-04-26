533039fcbe1ec97d4442f06e63f03b9aad568706
Tobit Misoi <misoitobit@zetech.ac.ke>
2021-04-26 12:08:34 +0300

    update

--- client/src/views/Blog.js
+++ client/src/views/Blog.js
@@ -1,13 +1,24 @@
+import { makeStyles } from '@material-ui/core'
 import React, { Component } from 'react'
+import BlogFetch from '../functions/BlogFetch'
 
-export class Blog extends Component {
-    render() {
-        return (
-            <div className="view">
-                Blog section
-            </div>
-        )
-    }
+const styles = {
+  container: {
+    border: '1px solid',
+    padding: '1rem'
+  }
 }
 
-export default Blog;
+const useStyles = makeStyles(styles)
+
+export function Blog() {
+  const classes = useStyles()
+
+  return (
+    <div className={classes.container}>
+      <BlogFetch />
+    </div>
+  )
+}
+
+export default Blog
